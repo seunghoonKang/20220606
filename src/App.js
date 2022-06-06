041d86b1d235227f4d44e832e3741349d77a3517
tmdgns3319 <tmdgns4321@gmail.com>
2022-06-06 14:13:23 +0900

    경로설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function HeaderTag() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+function NavlistTag() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello,WEB!
+    </article>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderTag></HeaderTag>
+      <NavlistTag></NavlistTag>
+      <Article></Article>
     </div>
   );
 }
