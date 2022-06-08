bff5d697933f02c1aa1641427ccf7ab20654c990
tmdgns3319 <tmdgns4321@gmail.com>
2022-06-08 14:48:15 +0900

    id에 따른 글의 내용이 변경되도록 개선

--- src/App.js
+++ src/App.js
@@ -1,45 +1,113 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-function HeaderTag() {
+function HeaderTag(props) {
   return (
     <header>
       <h1>
-        <a href="/">WEB</a>
+        <a
+          href="/"
+          onClick={(evt) => {
+            console.log("evt", evt);
+            evt.preventDefault();
+            props.onSelect();
+          }}
+        >
+          WEB
+        </a>
       </h1>
     </header>
   );
 }
-function NavlistTag() {
+function NavlistTag(props) {
+  //console.log(props.data);
+  const list = props.data.map((e) => {
+    return (
+      <li key={e.id}>
+        <a
+          href={"/read/" + e.id}
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect(e.id);
+          }}
+        >
+          {e.title}
+        </a>
+      </li>
+    );
+  });
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-      </ol>
+      <ol>{list}</ol>
     </nav>
   );
 }
 
-function Article() {
+function Article(props) {
+  // console.log(props);
   return (
     <article>
-      <h2>Welcome</h2>
-      Hello,WEB!
+      <h2>{props.title}</h2>
+      {props.body}
     </article>
   );
 }
 
 function App() {
+  let [mode, setMode] = useState("WELCOME");
+  let [id, setId] = useState(null);
+
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+  ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
+  } else if (mode === "READ") {
+    const topic = topics.filter((e) => {
+      if (e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    console.log(topic);
+    content = <Article title={topic.title} body={topic.body}></Article>;
+  }
+
   return (
     <div>
-      <HeaderTag></HeaderTag>
-      <NavlistTag></NavlistTag>
-      <Article></Article>
+      <HeaderTag
+        onSelect={() => {
+          // mode = "WELCOME";
+          setMode("WELCOME");
+        }}
+      ></HeaderTag>
+      <NavlistTag
+        data={topics}
+        onSelect={(id) => {
+          // mode = "READ";
+          setMode("READ");
+          setId(id);
+        }}
+      ></NavlistTag>
+      {content}
+      <ButtonGroup>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            alert("create!");
+          }}
+        >
+          Create
+        </Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
